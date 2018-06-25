b3d677f652b1be464062530d222977cfcebc39fc
Carl ERIKSSON <carl.eriksson@soprasteria.com>
2018-06-25 08:58:59 +0200

    Added all the files

---
+++ ClientApp/components/Monitor.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ApplicationState } from '../store';
+import * as DriveCollectionState from '../store/Drives';
+
+// At runtime, Redux will merge together...
+type DriveProps =
+    DriveCollectionState.DriveCollectionState        // ... state we've requested from the Redux store
+    & typeof DriveCollectionState.actionCreators      // ... plus action creators we've requested
+    & RouteComponentProps<{}>; // ... plus incoming routing parameters
+
+class Monitor extends React.Component<DriveProps, {}> {
+    componentWillMount() {
+        // This method runs when the component is first added to the page
+        this.props.loadDrives();
+    }
+
+    componentWillReceiveProps(nextProps: DriveProps) {
+        // This method runs when incoming props (e.g., route params) change
+        this.props.loadDrives();
+    }
+
+    public render() {
+        return <div>
+            <h1>Drive overview</h1>
+            <p>This component demonstrates the disk information on the client computer.</p>
+            {this.renderDriveCollection()}
+        </div>;
+    }
+
+    private renderDriveCollection() {
+        return <table className='table'>
+            <thead>
+                <tr>
+                    <th>Free space</th>
+                    <th>Format</th>
+                    <th>Type</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.props.drives.map(drive =>
+                    <tr key={drive.name}>
+                        <td>{drive.availableFreeSpace}</td>
+                        <td>{drive.driveFormat}</td>
+                        <td>{drive.driveType}</td>
+                        <td>{drive.name}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+    }
+
+  
+}
+
+export default connect(
+    (state: ApplicationState) => state.drives, // Selects which state properties are merged into the component's props
+    DriveCollectionState.actionCreators                 // Selects which action creators are merged into the component's props
+)(Monitor) as typeof Monitor;
---
+++ ClientApp/store/Drives.ts
@@ -0,0 +1,87 @@
+import { fetch, addTask } from 'domain-task';
+import { Action, Reducer, ActionCreator } from 'redux';
+import { AppThunkAction } from './';
+
+// -----------------
+// STATE - This defines the type of data maintained in the Redux store.
+
+export interface DriveCollectionState {
+    isCritical: boolean;
+    drives: DriveInfo[];
+}
+
+export interface DriveInfo {
+    availableFreeSpace: number;
+    driveFormat: string;
+    driveType: string;
+    name: string;
+    totalFreeSpace: number;
+    totalSize: number;
+    volumeLabel: string;
+    rootDirectory: string;
+}
+
+// -----------------
+// ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
+// They do not themselves have any side-effects; they just describe something that is going to happen.
+
+interface RequestDrivesAction {
+    type: 'REQUEST_DRIVES';
+    isCritical: boolean;
+}
+
+interface ReceiveDrivesAction {
+    type: 'RECEIVE_DRIVES';
+    isCritical: boolean;
+    drives: DriveInfo[];
+}
+
+// Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
+// declared type strings (and not any other arbitrary string).
+type KnownAction = RequestDrivesAction | ReceiveDrivesAction;
+
+// ----------------
+// ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
+// They don't directly mutate state, but they can have external side-effects (such as loading data).
+
+export const actionCreators = {
+    loadDrives: (): AppThunkAction<KnownAction> => (dispatch, getState) => {
+        // Only load data if it's something we don't already have (and are not already loading)
+        let fetchTask = fetch(`api/Monitor/LoadDrives`).then(response => response.json() as Promise<DriveInfo[]>)
+                .then(data => {
+                    dispatch({ type: 'RECEIVE_DRIVES', isCritical: true, drives: data });
+                });
+
+            addTask(fetchTask); // Ensure server-side prerendering waits for this to complete
+            dispatch({ type: 'REQUEST_DRIVES', isCritical: true });
+    }
+};
+
+// ----------------
+// REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
+
+const unloadedState: DriveCollectionState = { drives: [], isCritical: false };
+
+export const reducer: Reducer<DriveCollectionState> = (state: DriveCollectionState, incomingAction: Action) => {
+    const action = incomingAction as KnownAction;
+    switch (action.type) {
+        case 'REQUEST_DRIVES':
+            return {
+                isCritical: action.isCritical,
+                drives: state.drives,
+            };
+        case 'RECEIVE_DRIVES':
+            // Only accept the incoming data if it matches the most recent request. This ensures we correctly
+            // handle out-of-order responses.
+            return {
+                isCritical: action.isCritical,
+                drives: action.drives,
+                isLoading: false
+            };
+        default:
+            // The following line guarantees that every action in the KnownAction union has been covered by a case above
+            const exhaustiveCheck: never = action;
+    }
+
+    return state || unloadedState;
+};
