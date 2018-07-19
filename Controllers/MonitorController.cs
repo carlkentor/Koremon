8c2f6d0e7a2fd876401b1aef0243df021ecc6be7
Carl ERIKSSON <carl.eriksson@soprasteria.com>
2018-07-19 10:47:56 +0200

    model directory

--- Controllers/MonitorController.cs
+++ Controllers/MonitorController.cs
@@ -1,21 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Kormon.Controllers
 {
     [Route("api/[controller]")]
-
     public class MonitorController : Controller
     {
         [HttpGet("[action]")]
         public IEnumerable<DriveInfo> LoadDrives()
         {
-
-            return System.IO.DriveInfo.GetDrives().ToList().Select(x => new Kormon.Controllers.DriveInfo
+            return DriveInfo.GetDrives().ToList().Select(x => new Models.DriveInfo
             {
 
                 VolumeLabel = x.VolumeLabel,
@@ -26,127 +22,8 @@ namespace Kormon.Controllers
                 IsReady = x.IsReady,
                 TotalFreeSpace = x.TotalFreeSpace,
                 TotalSize = x.TotalSize,
-                //RootDirectory = x.RootDirectory //this fails to resolve in react, need to map the type RotDirectory to new object instead of string value :)
+                //RootDirectory = x.RootDirectory //React is unable to resolve this object, map the type RotDirectory to new object React friendly object instead
             });
         }
-    }
-
-    public class DriveInfo
-    {
-
-        //
-        // Summary:
-        //     Indicates the amount of available free space on a drive, in bytes.
-        //
-        // Returns:
-        //     The amount of free space available on the drive, in bytes.
-        //
-        // Exceptions:
-        //   T:System.UnauthorizedAccessException:
-        //     Access to the drive information is denied.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurred (for example, a disk error or a drive was not ready).
-        public long AvailableFreeSpace { get; set; }
-        //
-        // Summary:
-        //     Gets the name of the file system, such as NTFS or FAT32.
-        //
-        // Returns:
-        //     The name of the file system on the specified drive.
-        //
-        // Exceptions:
-        //   T:System.UnauthorizedAccessException:
-        //     Access to the drive information is denied.
-        //
-        //   T:System.IO.DriveNotFoundException:
-        //     The drive does not exist or is not mapped.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurred (for example, a disk error or a drive was not ready).
-        public string DriveFormat { get; set; }
-        //
-        // Summary:
-        //     Gets the drive type, such as CD-ROM, removable, network, or fixed.
-        //
-        // Returns:
-        //     One of the enumeration values that specifies a drive type.
-        public DriveType DriveType { get; set; }
-        //
-        // Summary:
-        //     Gets a value that indicates whether a drive is ready.
-        //
-        // Returns:
-        //     true if the drive is ready; false if the drive is not ready.
-        public bool IsReady { get; set; }
-        //
-        // Summary:
-        //     Gets the name of a drive, such as C:\.
-        //
-        // Returns:
-        //     The name of the drive.
-        public string Name { get; set; }
-        //
-        // Summary:
-        //     Gets the root directory of a drive.
-        //
-        // Returns:
-        //     An object that contains the root directory of the drive.
-        public DirectoryInfo RootDirectory { get; set; }
-        //
-        // Summary:
-        //     Gets the total amount of free space available on a drive, in bytes.
-        //
-        // Returns:
-        //     The total free space available on a drive, in bytes.
-        //
-        // Exceptions:
-        //   T:System.UnauthorizedAccessException:
-        //     Access to the drive information is denied.
-        //
-        //   T:System.IO.DriveNotFoundException:
-        //     The drive is not mapped or does not exist.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurred (for example, a disk error or a drive was not ready).
-        public long TotalFreeSpace { get; set; }
-        //
-        // Summary:
-        //     Gets the total size of storage space on a drive, in bytes.
-        //
-        // Returns:
-        //     The total size of the drive, in bytes.
-        //
-        // Exceptions:
-        //   T:System.UnauthorizedAccessException:
-        //     Access to the drive information is denied.
-        //
-        //   T:System.IO.DriveNotFoundException:
-        //     The drive is not mapped or does not exist.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurred (for example, a disk error or a drive was not ready).
-        public long TotalSize { get; set; }
-        //
-        // Summary:
-        //     Gets or sets the volume label of a drive.
-        //
-        // Returns:
-        //     The volume label.
-        //
-        // Exceptions:
-        //   T:System.IO.IOException:
-        //     An I/O error occurred (for example, a disk error or a drive was not ready).
-        //
-        //   T:System.IO.DriveNotFoundException:
-        //     The drive is not mapped or does not exist.
-        //
-        //   T:System.Security.SecurityException:
-        //     The caller does not have the required permission.
-        //
-        //   T:System.UnauthorizedAccessException:
-        //     The volume label is being set on a network or CD-ROM drive. -or- Access to the
-        //     drive information is denied.
-        public string VolumeLabel { get; set; }
     }
 }
